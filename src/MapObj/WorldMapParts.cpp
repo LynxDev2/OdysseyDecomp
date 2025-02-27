7d50088b7adbfe5c26bf3a14964b8f82fb2fbf12
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-02-27 19:27:41 +0200

    Test5

--- src/MapObj/WorldMapParts.cpp
+++ src/MapObj/WorldMapParts.cpp
@@ -21,7 +21,7 @@ void WorldMapParts::setWorldMtx(const sead::Matrix34f& srcMtx) {
 
     inverse.setInverse(*mWorldMtx);
     copyMtx.setMul(inverse, srcMtx);
-    setLocalMtx(copyMtx);a
+    setLocalMtx(copyMtx);
 }
 
 void WorldMapParts::updatePose() {
