fbd59b7e18216063fc8aea8c9bcad16e713160a1
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-10-19 22:30:57 +0300

    Try to fix pipe fail

--- .github/scripts/test-compile-create.sh
+++ .github/scripts/test-compile-create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # bash boilerplate
-set -euo pipefail # strict mode
+set -eo pipefail # strict mode
 readonly SCRIPT_NAME="$(basename "$0")"
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 function l { # Log a message to the terminal.
