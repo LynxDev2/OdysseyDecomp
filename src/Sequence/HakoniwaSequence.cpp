02f2ea5afd0b0144e46dc737008ae9f42e2e6537
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2026-01-10 20:47:08 +0200

    Custom speedboot

--- src/Sequence/HakoniwaSequence.cpp
+++ src/Sequence/HakoniwaSequence.cpp
@@ -5,9 +5,21 @@
 #include <gfx/seadViewport.h>
 
 #include "Library/Layout/LayoutUtil.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+#include "Library/Play/Layout/WipeHolder.h"
 #include "Library/Screen/ScreenFunction.h"
 #include "Library/Sequence/Sequence.h"
 #include "Library/System/GameSystemInfo.h"
+#include "Library/Thread/AsyncFunctorThread.h"
+
+#include "Sequence/WorldResourceLoader.h"
+#include "System/GameDataFunction.h"
+#include "System/WorldList.h"
+
+NERVE_IMPL(HakoniwaSequence, LoadStage);
+
+NERVES_MAKE_NOSTRUCT(HakoniwaSequence, LoadStage);
 
 void HakoniwaSequence::drawMain() const {
     al::Sequence::drawMain();
@@ -25,4 +37,30 @@ void HakoniwaSequence::drawMain() const {
     al::executeDraw(mLayoutKit, "２Ｄバック（メイン画面）");
     al::executeDraw(mLayoutKit, "２Ｄベース（メイン画面）");
     al::executeDraw(mLayoutKit, "２Ｄオーバー（メイン画面）");
+}
+
+// CUSTOM SpeedBoot with code addpated from:  https://github.com/Amethyst-szs/shadow-clone-chasers
+bool hasStartedLoadingResource = false;
+
+void HakoniwaSequence::exeBootLoadData() {
+    if (al::isFirstStep(this))
+        mInitThread->start();
+    if (!mInitThread->isDone())
+        return;
+    if (!hasStartedLoadingResource) {
+        const char* name = GameDataFunction::getNextStageName(mGameDataHolderAccessor);
+        if (!name)
+            name = GameDataFunction::getMainStageName(mGameDataHolderAccessor, 0);
+        int scenario = GameDataFunction::calcNextScenarioNo(mGameDataHolderAccessor);
+        if (scenario == -1)
+            scenario = 1;
+        int world = mGameDataHolderAccessor->getWorldList()->tryFindWorldIndexByStageName(name);
+        if (world > -1)
+            mResourceLoader->requestLoadWorldHomeStageResource(world, scenario);
+        hasStartedLoadingResource = true;
+        // This wipe is automatically opened by exePlayStage
+        mWipeHolder->startClose("FadeWhite", -1);
+    }
+    if (mResourceLoader->isEndLoadWorldResource() && mWipeHolder->isCloseEnd())
+        al::setNerve(this, &LoadStage);
 }
