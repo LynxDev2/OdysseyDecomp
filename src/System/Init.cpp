383c3865d81c5d48d55f430232d652aa28ddd602
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2026-01-11 19:09:03 +0200

    Add nso target and ifdefed nninitStartup patch

--- src/System/Init.cpp
+++ src/System/Init.cpp
@@ -8,13 +8,17 @@
 
 extern "C" void nninitStartup() {
     uintptr_t allocatorHeap;
-    uintptr_t recordingHeap;
+    [[maybe_unused]] uintptr_t recordingHeap;
 
-    nn::os::SetMemoryHeapSize(0xc8000000);
+    #ifdef NO_HEAP_PATCH
+        nn::os::SetMemoryHeapSize(0xc8000000);
+        nn::os::AllocateMemoryBlock(&recordingHeap, 0x6000000);
+        nn::oe::EnableGamePlayRecording(reinterpret_cast<void*>(recordingHeap), 0x6000000);
+    #else
+        nn::os::SetMemoryHeapSize(0xc3000000);
+    #endif
     nn::os::AllocateMemoryBlock(&allocatorHeap, 0x2400000);
     nn::init::InitializeAllocator(reinterpret_cast<void*>(allocatorHeap), 0x2400000);
-    nn::os::AllocateMemoryBlock(&recordingHeap, 0x6000000);
-    nn::oe::EnableGamePlayRecording(reinterpret_cast<void*>(recordingHeap), 0x6000000);
 }
 
 extern "C" void nnMain() {
